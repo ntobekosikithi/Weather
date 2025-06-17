59811f06bb9e3872d7eacb4f85a4bfaa528cdf8a
Ntobeko Sikithi <ntobekosikithi@gmail.com.com>
2025-06-17 10:29:03 +0200

    Update CurrentWeatherCard.swift

--- Sources/Weather/Views/CurrentWeatherCard.swift
+++ Sources/Weather/Views/CurrentWeatherCard.swift
@@ -29,7 +29,7 @@ struct CurrentWeatherCard: View {
                     emptyStateView
                 }
             }
-            .frame(height: 200)
+            .frame(height: 400)
         }
         .padding()
         .background(Color(.systemGray6))
